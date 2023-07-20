45ba8be722b42a1a1d3585bb7f14efe9672113aa
theaug <93984863+vuthaison1308@users.noreply.github.com>
2023-07-20 09:06:48 +0700

    Update B2BAI00.cpp

--- B2BAI00.cpp
+++ B2BAI00.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n, k, i, ans=1;
+long long n, k, ans=1;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     freopen("B2BAI00.INP", "r", stdin);
     freopen("B2BAI00.OUT", "w", stdout);
     cin >> n >> k;
-    while (n--){
-        cin >> i;
-        ans*=i%k;
+    long long a[n+1];
+    for (long long i = 1; i <= n; i++) cin >> a[i];
+    for (long long i = 1; i <= n; i++){
+        ans=(ans*(a[i]%k))%k;
     }
-    cout << ans%k;
+    cout << ans;
 }
